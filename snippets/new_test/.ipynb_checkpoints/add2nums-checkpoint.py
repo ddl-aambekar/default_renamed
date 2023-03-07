da8f64098e04c48061f6691c5473d319da941b2a
Akshay Ambekar <akshay.ambekar@dominodatalab.com>
2023-03-07 12:16:05 -0800

    Pushed from Domino: https://prod-field.cs.domino.tech/workspace/ddl-akshay/NEW_GBP_model_test?executionId=6406b2a546197615f41caaad

---
+++ snippets/new_test/.ipynb_checkpoints/add2nums-checkpoint.py
@@ -0,0 +1,2 @@
+def add_2_numbers(num1, num2):
+    return num1+num2
---
+++ snippets/new_test/add2nums.py
@@ -0,0 +1,5 @@
+def add_2_numbers(num1, num2):
+    # do some cool stuff here
+    # and more stuff here
+    # and then finally save this
+    return num1+num2
